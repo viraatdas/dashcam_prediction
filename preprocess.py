8f9f7d5b7e843aff2f71846c77d3400b1168cdbe
Viraat Das <viraat.laldas@gmail.com>
2019-05-13 13:57:38 -0400

    Added notation for csv; speed and time

--- preprocess.py
+++ preprocess.py
@@ -11,14 +11,29 @@ def dispVid(cap):
 
 #Read training videp
 cap = cv.VideoCapture("data/train.mp4")
+speed = []
+with open("data/train.txt") as f:
+    for el in f:
+        speed.append(float(el.rstrip('\n')))
+iter_speed = iter(speed)
 #Cropping video feed
 x_start = 0
 x_end = 640
 y_start = 160
 y_end = 370
+
+fps = cap.get(cv.CAP_PROP_FRAME_COUNT)
+frame_count = 0
+csv_write = []
 while True:
     ret, frame = cap.read()
     frame = frame[y_start:y_end, x_start:x_end]
+    frame_count+=1
+    time = float(frame_count)/fps
+    file_loc = "data/train_image/train_" + str(time) + ".jpg"
+    cv.imwrite(file_loc, frame)
+    string = file_loc + "," + str(time) + "," + str(next(iter_speed))
+    csv_write.append(string)
     cv.imshow("Feed", frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
