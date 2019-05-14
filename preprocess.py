24085884f633cdba3493e8baccb41d7305f47bbd
Viraat Das <viraat.laldas@gmail.com>
2019-05-14 12:58:36 -0400

    Name cleanup

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,16 @@
-import cv2 as cv
-from matplotlib import pyplot as plt
+import cv2
 import pickle
 
 #Function to display the video of the current capture
 def dispVid(cap):
     while True:
         ret, frame = cap.read()
-        cv.imshow("Name", frame)
-        if cv.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow("Name", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 #Read training videp
-cap = cv.VideoCapture("data/train.mp4")
+cap = cv2.VideoCapture("data/train.mp4")
 speed = []
 with open("data/train.txt") as f:
     for el in f:
@@ -23,7 +22,7 @@ x_end = 640
 y_start = 160
 y_end = 370
 
-fps = cap.get(cv.CAP_PROP_FRAME_COUNT)
+fps = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 frame_count = 0
 csv_write = []
 while True:
@@ -32,15 +31,15 @@ while True:
         frame = frame[y_start:y_end, x_start:x_end]
         frame_count+=1
         file_loc = "data/train_image/train_" + str(frame_count) + ".jpg"
-        cv.imwrite(file_loc, frame)
+        cv2.imwrite(file_loc, frame)
         string = file_loc + "," + str(frame_count) + "," + str(next(iter_speed))
         csv_write.append(string)
-        if cv.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     except:
         break
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
 
 
 output = open('data.pkl', 'wb')
