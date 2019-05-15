3f4882bb227b19b229f01d296f8dfcf78558a5e4
Viraat Das <viraat.laldas@gmail.com>
2019-05-15 03:08:28 -0400

    full toss copy/paste

--- model.py
+++ model.py
@@ -27,6 +27,136 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 seeds = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
 
 
+def train_valid_split(dframe, seed_val):
+    """
+    Randomly shuffle pairs of rows in the dataframe, separates train and validation data
+    generates a uniform random variable 0->9, gives 20% chance to append to valid data, otherwise train_data
+    return tuple (train_data, valid_data) dataframes
+    """
+    train_data = pd.DataFrame()
+    valid_data = pd.DataFrame()
+    np.random.seed(seed_val)
+    for i in tqdm(range(len(dframe) - 1)):
+        idx1 = np.random.randint(len(dframe) - 1)
+        idx2 = idx1 + 1
+
+        row1 = dframe.iloc[[idx1]].reset_index()
+        row2 = dframe.iloc[[idx2]].reset_index()
+
+        randInt = np.random.randint(9)
+        if 0 <= randInt <= 1:
+            valid_frames = [valid_data, row1, row2]
+            valid_data = pd.concat(valid_frames, axis=0, join='outer', ignore_index=False)
+        if randInt >= 2:
+            train_frames = [train_data, row1, row2]
+            train_data = pd.concat(train_frames, axis=0, join='outer', ignore_index=False)
+    return train_data, valid_data
+
+train_meta = pd.read_csv("data/img_csv/train_info.csv")
+
+print('shape: ', train_meta.shape)
+
+train_data, valid_data = train_valid_split(train_meta, seeds[0])
+
+fig, ax = plt.subplots(figsize=(20,10))
+plt.plot(train_data.sort_values(['image_index'])[['image_index']], train_data.sort_values(['image_index'])[['speed']], 'ro')
+plt.plot(valid_data.sort_values(['image_index'])[['image_index']], valid_data.sort_values(['image_index'])[['speed']], 'go')
+plt.xlabel('image_index (or time since start)')
+plt.ylabel('speed')
+plt.title('Speed vs time')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('./assets/speed_vs_time_val_train.png')
+plt.close()
+
+print('----')
+print('valid_data: ', valid_data.shape)
+print('train_data: ', train_data.shape)
+
+
+def change_brightness(image, bright_factor):
+    """
+    Augments the brightness of the image by multiplying the saturation by a uniform random variable
+    Input: image (RGB)
+    returns: image with brightness augmentation
+    """
+
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    # perform brightness augmentation only on the second channel
+    hsv_image[:, :, 2] = hsv_image[:, :, 2] * bright_factor
+
+    # change back to RGB
+    image_rgb = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2RGB)
+    return image_rgb
+
+def preprocess_image_valid_from_path(image_path, speed):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = preprocess_image(img)
+    return img, speed
+
+
+def preprocess_image_from_path(image_path, speed, bright_factor):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = change_brightness(img, bright_factor)
+    img = preprocess_image(img)
+    return img, speed
+
+
+def opticalFlowDense(image_current, image_next):
+    """
+    input: image_current, image_next (RGB images)
+    calculates optical flow magnitude and angle and places it into HSV image
+    * Set the saturation to the saturation value of image_next
+    * Set the hue to the angles returned from computing the flow params
+    * set the value to the magnitude returned from computing the flow params
+    * Convert from HSV to RGB and return RGB image with same size as original image
+    """
+    gray_current = cv2.cvtColor(image_current, cv2.COLOR_RGB2GRAY)
+    gray_next = cv2.cvtColor(image_next, cv2.COLOR_RGB2GRAY)
+
+    hsv = np.zeros((66, 220, 3))
+    # set saturation
+    hsv[:, :, 1] = cv2.cvtColor(image_next, cv2.COLOR_RGB2HSV)[:, :, 1]
+
+    # Flow Parameters
+    # flow_mat = cv2.CV_32FC2
+    flow_mat = None
+    image_scale = 0.5
+    nb_images = 1
+    win_size = 15
+    nb_iterations = 2
+    deg_expansion = 5
+    STD = 1.3
+    extra = 0
+
+    # obtain dense optical flow paramters
+    flow = cv2.calcOpticalFlowFarneback(gray_current, gray_next,
+                                        flow_mat,
+                                        image_scale,
+                                        nb_images,
+                                        win_size,
+                                        nb_iterations,
+                                        deg_expansion,
+                                        STD,
+                                        0)
+
+    # convert from cartesian to polar
+    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+
+    # hue corresponds to direction
+    hsv[:, :, 0] = ang * (180 / np.pi / 2)
+
+    # value corresponds to magnitude
+    hsv[:, :, 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+
+    # convert HSV to float32's
+    hsv = np.asarray(hsv, dtype=np.float32)
+    rgb_flow = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
+    return rgb_flow
+
+
 def preprocess_image(image):
     """
     preprocesses the image
@@ -61,121 +191,27 @@ def preprocess_image_from_path(image_path, speed, bright_factor):
     img = preprocess_image(img)
     return img, speed
 
-def train_valid_split(dframe, seed_val):
-    """
 
-    :param dframe:
-    :param seed_val:
-    :return: tuple(train_data, valid_data) df
-
-    Shuffle pairs of rows in the df, and seperates train and validation data
-    """
-
-    train_data = pd.DataFrame()
-    valid_data = pd.DataFrame()
-
-    np.random.seed(seed_val)
-    for i in tqdm(range(len(dframe) - 1)):
-        idx1 = np.random.randint(len(dframe) - 1)
-        idx2 = idx1 + 1
-
-        row1 = dframe.iloc[[idx1]].reset_index()
-        row2 = dframe.iloc[[idx2]].reset_index()
-
-        randInt = np.random.randint(9)
-        if 0 <= randInt <= 1:
-            valid_frames = [valid_data, row1, row2]
-            valid_data = pd.concat(valid_frames, axis = 0, join = 'outer', ignore_index=False)
-        if randInt >= 2:
-            train_frames = [train_data, row1, row2]
-            train_data = pd.concat(train_frames, axis = 0, join = 'outer', ignore_index=False)
-        return train_data, valid_data
-train_meta = pd.read_csv("data/img_csv/train_info.csv")
-
-print('shape: ', train_meta.shape)
-print (train_meta)
-
-train_data, valid_data = train_valid_split(train_meta, seeds[0])
-
-fig, ax = plt.subplots(figsize=(20,10))
-plt.plot(train_data.sort_values(['image_index'])[['image_index']], train_data.sort_values(['image_index'])[['speed']], 'ro')
-plt.plot(valid_data.sort_values(['image_index'])[['image_index']], valid_data.sort_values(['image_index'])[['speed']], 'go')
-plt.xlabel('image_index (or time since start)')
-plt.ylabel('speed')
-plt.title('Speed vs time')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.savefig('./assets/speed_vs_time_val_train.png')
-plt.close()
-
-print('----')
-print('valid_data: ', valid_data.shape)
-print('train_data: ', train_data.shape)
-
-
-
-
-def opticalFlowDense(curr_img, next_img):
-    gray_current = cv2.cvtColor(curr_img, cv2.COLOR_RGB2GRAY)
-    gray_next = cv2.cvtColor(next_img, cv2.COLOR_RGB2GRAY)
-
-    hsv = np.zeros((66, 220, 3))
-
-    #set saturation
-    hsv[:,:,1] = cv2.cvtCOLOR(next_img, cv2.COLOR_RGB2HSV)[:,:,1]
-
-    #Flow Parameters
-    flow_mat = None
-    image_scale = 0.5
-    nb_images = 1
-    win_size = 15
-    nb_iterations = 2
-    deg_expansion = 5
-    STD = 1.3
-    extra = 0
-
-    #dense optical flow parameters
-    flow = cv2.calcOpticalFlowFarneback(gray_current, gray_next,
-                                        flow_mat,
-                                        image_scale,
-                                        nb_images,
-                                        win_size,
-                                        nb_iterations,
-                                        deg_expansion,
-                                        STD,
-                                        0)
-
-    #convert from cartesian to polar
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-
-    #hue corresponds to direction
-    hsv[:,:,0] = ang * (180/ np.pi / 2)
-
-    #value corresponds to magnitude
-    hsv[:,:,2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-
-    #convert HSV to int32's
-    hsv = np.asarray(hsv, dtype = np.float32)
-    rgb_flow = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-
-    return rgb_flow
-
-
-def generate_training_data(data, batch_size = 32):
-    image_batch = np.zeros((batch_size, 66, 220, 3)) # nvidia input params
+def generate_training_data(data, batch_size=32):
+    image_batch = np.zeros((batch_size, 66, 220, 3))  # nvidia input params
     label_batch = np.zeros((batch_size))
     while True:
         for i in range(batch_size):
+            # generate a random index with a uniform random distribution from 1 to len - 1
             idx = np.random.randint(1, len(data) - 1)
+
+            # Generate a random bright factor to apply to both images
+            bright_factor = 0.2 + np.random.uniform()
 
             row_now = data.iloc[[idx]].reset_index()
             row_prev = data.iloc[[idx - 1]].reset_index()
             row_next = data.iloc[[idx + 1]].reset_index()
 
-            # Find the 3 respective times to determine frame order
+            # Find the 3 respective times to determine frame order (current -> next)
 
             time_now = row_now['image_index'].values[0]
             time_prev = row_prev['image_index'].values[0]
-            time_next = row_next['imagE_index'].values[0]
+            time_next = row_next['image_index'].values[0]
 
             if abs(time_now - time_prev) == 1 and time_now > time_prev:
                 row1 = row_prev
@@ -185,7 +221,7 @@ def generate_training_data(data, batch_size = 32):
                 row1 = row_now
                 row2 = row_next
             else:
-                print("Error generating row")
+                print('Error generating row')
 
             x1, y1 = preprocess_image_from_path(row1['image_path'].values[0],
                                                 row1['speed'].values[0],
@@ -205,9 +241,9 @@ def generate_training_data(data, batch_size = 32):
             image_batch[i] = rgb_diff
             label_batch[i] = y
 
-    # print('image_batch', image_batch.shape, ' label_batch', label_batch)
-    # Shuffle the pairs before they get fed into the network
-    yield shuffle(image_batch, label_batch)
+        # print('image_batch', image_batch.shape, ' label_batch', label_batch)
+        # Shuffle the pairs before they get fed into the network
+        yield shuffle(image_batch, label_batch)
 
 
 def generate_validation_data(data):
@@ -356,4 +392,4 @@ plt.legend(['training set', 'validation set'], loc='upper right')
 plt.savefig('./assets/MSE_per_epoch.png')
 plt.close()
 
-print("done")
+print('done')
