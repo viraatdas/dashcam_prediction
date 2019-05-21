df1eb14432710cc96f2ea8c69a03ff23c5e9b829
Viraat Das <viraat.laldas@gmail.com>
2019-05-21 18:07:10 -0400

    updated the code; run on cluster

--- predict.py
+++ predict.py
@@ -23,6 +23,7 @@ import keras.backend.tensorflow_backend as KTF
 
 from model import *
 
+
 def make_predictions(data):
     for idx in tqdm(range(1, len(data.index) - 1)):
         row_now = data.iloc[[idx]].reset_index()
@@ -110,6 +111,75 @@ def error_analysis(error_thresh):
     plt.show()
 
 
+# Predicting on Test
+test_meta = pd.read_csv('data/test_info.csv')
+print('shape: ', test_meta.shape)
+
+make_predictions(test_meta)
+
+window_size = 25
+test_meta['smooth_predicted_speed'] = pd.rolling_median(test_meta['predicted_speed'], window_size, center=True)
+test_meta['smooth_error'] = test_meta.apply(lambda x: x['smooth_predicted_speed'] - x['speed'], axis=1)
+
+test_meta['smooth_predicted_speed'] = test_meta.apply(lambda x:
+                                                        x['predicted_speed'] if np.isnan(x['smooth_predicted_speed'])
+                                                       else x['smooth_predicted_speed'],axis=1)
+
+test_meta['smooth_error'] = test_meta.apply(lambda x: x['error'] if np.isnan(x['smooth_error'])
+                                                       else x['smooth_error'],axis=1)
+
+
+fig, ax = plt.subplots(figsize=(20,10))
+plt.plot(test_meta.sort_values(['image_index'])[['image_index']],
+         test_meta.sort_values(['image_index'])[['predicted_speed']], 'bx')
+plt.plot(test_meta.sort_values(['image_index'])[['image_index']],
+         test_meta.sort_values(['image_index'])[['smooth_predicted_speed']], 'g.')
+plt.xlabel('image_index (or time since start)')
+plt.ylabel('speed')
+plt.title('Predicted on test data')
+plt.legend(['test speed', 'predicted speed'], loc='upper right')
+plt.show()
+plt.close()
+
+output_file = test_meta['smooth_predicted_speed']
+output_file.to_csv('data/test.txt', index=False)
+
+output_file.head()
+
+# Predicting on Train
+
+make_predictions(train_meta)
+
+train_meta.head()
+
+
+window_size = 25
+train_meta['smooth_predicted_speed'] = pd.rolling_median(train_meta['predicted_speed'], window_size, center=True)
+train_meta['smooth_error'] = train_meta.apply(lambda x: x['smooth_predicted_speed'] - x['speed'], axis=1)
+
+train_meta['smooth_predicted_speed'] = train_meta.apply(lambda x:
+                                                        x['predicted_speed'] if np.isnan(x['smooth_predicted_speed'])
+                                                       else x['smooth_predicted_speed'],axis=1)
+
+train_meta['smooth_error'] = train_meta.apply(lambda x: x['error'] if np.isnan(x['smooth_error'])
+                                                       else x['smooth_error'],axis=1)
+
+fig, ax = plt.subplots(figsize=(20,10))
+plt.plot(train_meta.sort_values(['image_index'])[['image_index']],
+         train_meta.sort_values(['image_index'])[['predicted_speed']], 'bx')
+plt.plot(train_meta.sort_values(['image_index'])[['image_index']],
+         train_meta.sort_values(['image_index'])[['smooth_predicted_speed']], 'g.')
+plt.plot(train_meta.sort_values(['image_index'])[['image_index']],
+         train_meta.sort_values(['image_index'])[['speed']], 'r.')
+plt.xlabel('image_index (or time since start)')
+plt.ylabel('speed')
+plt.title('Predicted vs actual on train data')
+plt.legend(['predicted speed', (str(window_size) +' window average'), 'ground truth'], loc='upper right')
+plt.show()
+plt.close()
+
+print(mean_squared_error(train_meta.iloc[1:-1, 2],
+                         train_meta.iloc[1:-1, 5]))
 
 
 
